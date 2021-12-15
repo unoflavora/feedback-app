dd99c5274b60e1228f0c0bc65e0e6966e5f4a001
unoflavora <imamsyahid@diktus.id>
2021-12-15 10:38:35 +0700

    change utils

--- frontend/src/utils/feedbackUtils.js
+++ frontend/src/utils/feedbackUtils.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'http://localhost:3000'
+const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : ''
 let token
 
 const fetch = async () => {
